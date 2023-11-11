9cee5173d55d58b728efd2f49d3e9968e764eb84
Charlie Arriaga <arriayaa@gmail.com>
2023-11-10 16:42:41 -0800

    made to do list

---
+++ js/app.js
@@ -0,0 +1,23 @@
+/* TO DO
+
+Point system: Conditional statement?
+
+Wireframe 1 rendering
+Event listeners
+
+Wireframe 2 rendering
+Event listeners
+
+Wireframe 3 rendering
+Event listeners
+
+Wireframe 4 rendering
+
+Two functions for rendering
+
+Conditional statement for round numbers: if or switch
+
+Results page: local storage
+
+
+*/
