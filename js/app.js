3f08d5d92f66d3de217d95923407d54c5cd10ade
Charlie Arriaga <arriayaa@gmail.com>
2023-11-13 12:18:55 -0800

    added homepage

--- js/app.js
+++ js/app.js
@@ -21,3 +21,35 @@ Results page: local storage
 
 
 */
+
+// h1 header
+// button
+// h2 header
+
+const startButton = document.createElement('button');
+makeElement(startButton, 'startButton');
+startButton.textContent = 'start';
+startButton.addEventListener('click', initGame)
+
+function makeElement (element, parentElementIdString) {
+    parentElement = document.getElementById(parentElementIdString);
+    parentElement.appendChild(element);
+}
+
+
+function homepage () {
+    const h1 = document.createElement('h1');
+    makeElement(h1, 'header');
+    h1.textContent = 'CHOOSE YOUR FATE';
+
+
+    const h2 = document.createElement('h2');
+    makeElement(h2, 'header2');
+    h2.textContent = 'Coding Bootcamp: Enter If You Dare';
+}
+
+function initGame () {
+
+}
+
+homepage();
